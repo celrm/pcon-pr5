9be1be00465f43cd1a8841c9c67af1aa8b400fcf
Celia Rubio <celia_rubio@hotmail.es>
2021-05-11 14:55:50 +0200

    5.1

--- 5.1/src/Cliente.java
+++ 5.1/src/Cliente.java
@@ -1,10 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Cliente {
 
-	public static void main(String[] args) {
-		// Enviar petición al servidor:
-		// nombre de fichero
-		// esperar contenidos
+	public static void main(String[] args) throws UnknownHostException, IOException {
+	System.out.println("Esperando conexión..."); System.out.flush();
+		Socket s = new Socket("localhost",501);
+	System.out.println("Conectado"); System.out.flush();
+
+		PrintWriter foutc = new PrintWriter(s.getOutputStream());
+		foutc.println("f1.txt"); foutc.flush();
+	System.out.println("Mandada petición"); System.out.flush();		
+		
+		BufferedReader finc = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		char c = (char) finc.read();
+		while (c != (char)0) {
+			System.out.print(c);
+			c = (char) finc.read();
+		}
+		System.out.println();
+		
+		s.close();
+	System.out.println("Conexión terminada"); System.out.flush();
 	}
 
 }
--- 5.1/src/Servidor.java
+++ 5.1/src/Servidor.java
@@ -1,14 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		@SuppressWarnings("resource")
+		ServerSocket ss = new ServerSocket(501);
+
 		while(true) {
-			// espera conexión
-			// recibe conexión
-			// establece socket:
-			// recibe nombre de fichero a obtener
-			// envía contenido a través del flujo de salida asociado al socket
+		System.out.println("Esperando conexión..."); System.out.flush();
+			Socket s = ss.accept();
+		System.out.println("Conectado"); System.out.flush();
+			
+			BufferedReader fin = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String fichero = fin.readLine();
+		System.out.println("He recibido fichero " + fichero); System.out.flush();
+
+			try {
+				File file = new File(fichero);
+			    Scanner lectura = new Scanner(file);
+			    String dentro = "";
+			    while (lectura.hasNextLine()) {
+			    	dentro = dentro + "\n" + lectura.nextLine();
+			    }
+			    dentro = dentro.substring(1); // droppeo el '\n'
+			    lectura.close();
+		System.out.println("Contenido:"); System.out.flush();
+		System.out.println(dentro); System.out.flush();
+		
+				PrintWriter fout = new PrintWriter(s.getOutputStream());
+		        fout.print(dentro); fout.print((char)0);
+		        fout.flush();
+		System.out.println("Mandado contenido"); System.out.flush();
+			} catch (FileNotFoundException e) {
+		System.out.println("No fichero " + fichero); System.out.flush();
+			    e.printStackTrace();
+			}
 		}
 	}
-
 }
